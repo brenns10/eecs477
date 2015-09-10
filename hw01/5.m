bb065a35db5b6b9e169c60d8616b3a880e7d88c9
Stephen Brennan <smb196@case.edu>
2015-09-10 15:00:20 -0400

    HW01: Finished 5.

---
+++ hw01/5.m
@@ -0,0 +1,9 @@
+c = [30; 20; 100; 90; 160];
+A = [5, 10, 20, 30, 40];
+b = [60];
+lb = [];
+ub = [];
+ctype = "U";
+vartype = "IIIII";
+sense = -1;
+[xmax, fmax, status, extra] = glpk(c, A, b, lb, ub, ctype, vartype, sense)
